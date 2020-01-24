5894de20fde73d7d271542db043a1f2f8269d07d
ramon <dsrmaniaco@gmail.com>
2020-01-24 23:11:06 +0100

    Prediction added, just need to append it to dataframe

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # Library Imports
+import Tkinter
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.svm import SVR
 
 # Loading/Reading in the Data
 df = pd.read_csv("BTC-USD.csv")
@@ -21,13 +24,15 @@ prediction_days = 30 #n = 30 days
 
 df['Price'] = (df['Open'] + df['Close'])/2
 
+
+
+
 #Remove temporal columns
 df.drop(['Open'], 1, inplace=True)
 df.drop(['Close'], 1, inplace=True)
 
 #Create another column for predictions, we need to shift it up
 df['Prediction'] = df[['Price']].shift(-prediction_days)
-
 #Converting it to numpy array 
 
 # Convert the dataframe to a numpy array and drop the prediction column
@@ -42,3 +47,34 @@ y = np.array(df['Prediction'])
 # Get all of the y values except the last 'n' rows
 y = y[:-prediction_days]
 
+#Prediction part
+
+# Split the data into 80% training and 20% testing
+
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+# Set prediction_days_array equal to the last 30 rows of the original data set from the price column
+prediction_days_array = np.array(df.drop(['Prediction'],1))[-prediction_days:]
+
+# Create and train the Support Vector Machine (Regression) using the radial basis function
+svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.00001) #typical gamma
+svr_rbf.fit(x_train, y_train) # fitting the model
+
+# Testing Model: Score returns the accuracy of the prediction. 
+# The best possible score is 1.0
+svr_rbf_confidence = svr_rbf.score(x_test, y_test)
+print("Accuracy: ", svr_rbf_confidence)
+
+# Print the predicted value
+svm_prediction = svr_rbf.predict(x_test)
+
+# Print the model predictions for the next 'n=30' days
+svm_prediction = svr_rbf.predict(prediction_days_array)
+
+#Print the actual price for the next 'n' days, n=prediction_days=30 
+df.dropna(inplace=True)
+
+df.plot(y='Price',kind='line')
+df.plot(y='Prediction',kind='line')
+
+plt.show()
